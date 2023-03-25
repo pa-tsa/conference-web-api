07ec86db22b0c1a52c2b2ed9301d82157b87d387
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-03-25 10:58:08 -0400

    Created initial app shell
    Provides base framework for security and status checks
    Provides Swagger for development purposes only
    Provides ReSharper settings and .editorconfig

---
+++ src/PaTsa.Conference.App.Api.WebApi/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PaTsa.Conference.App.Api.WebApi.Models;
+
+namespace PaTsa.Conference.App.Api.WebApi.Middleware;
+
+public class ApiKeyMiddleware
+{
+    public const string ApiHeaderKey = "X-API-Key";
+    public const string ApiKeyConfigValue = "ApiKey";
+
+    private readonly RequestDelegate _requestDelegate;
+
+    public ApiKeyMiddleware(RequestDelegate requestDelegate)
+    {
+        _requestDelegate = requestDelegate;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Path.HasValue &&
+            context.Request.Path.Value.Equals("/api/status", StringComparison.InvariantCultureIgnoreCase))
+        {
+            await _requestDelegate(context);
+            return;
+        }
+
+        var configuration = context.RequestServices.GetRequiredService<IConfiguration>();
+
+        var apiKey = configuration.GetValue<string>(ApiKeyConfigValue);
+
+        if (!context.Request.Headers.TryGetValue(ApiHeaderKey, out var sentApiKey) ||
+            string.IsNullOrWhiteSpace(sentApiKey) ||
+            sentApiKey != apiKey)
+        {
+            SetUnauthorizedResponse(context.Response);
+            return;
+        }
+
+        await _requestDelegate(context);
+    }
+
+    private static async void SetUnauthorizedResponse(HttpResponse httpResponse)
+    {
+        httpResponse.StatusCode = 401;
+        await httpResponse.WriteAsJsonAsync(ApiErrorModel.Unauthorized);
+    }
+}
---
+++ src/PaTsa.Conference.App.Api.WebApi/Services/IPingableService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace PaTsa.Conference.App.Api.WebApi.Services;
+
+public interface IPingableService
+{
+    string ServiceName { get; }
+
+    Task<bool> PingAsync(CancellationToken cancellationToken = default);
+}
