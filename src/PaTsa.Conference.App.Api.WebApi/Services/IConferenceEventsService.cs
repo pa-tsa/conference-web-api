2a9831a0cf6dafc5a0f6804708fc9a37c33f7996
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-03-30 09:08:14 -0400

    Created ConferenceEventsService

---
+++ src/PaTsa.Conference.App.Api.WebApi/Services/IConferenceEventsService.cs
@@ -0,0 +1,5 @@
+﻿using PaTsa.Conference.App.Api.WebApi.Entities;
+
+namespace PaTsa.Conference.App.Api.WebApi.Services;
+
+public interface IConferenceEventsService : IMongoEntityService<ConferenceEvent>, IPingableService { }
