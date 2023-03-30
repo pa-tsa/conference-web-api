ecab956d88f274275f033ccc4d40df12e35b47e8
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-03-30 09:05:36 -0400

    Added configuration classes

---
+++ src/PaTsa.Conference.App.Api.WebApi/Configuration/ConferenceDatabase.cs
@@ -0,0 +1,10 @@
+﻿// If the values are null, that means the app was miss configured
+// If the app was miss configured, let the error bubble up
+
+#pragma warning disable CS8618
+namespace PaTsa.Conference.App.Api.WebApi.Configuration;
+
+public class ConferenceDatabase
+{
+    public string DatabaseName { get; set; }
+}
---
+++ src/PaTsa.Conference.App.Api.WebApi/Configuration/ConfigurationKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace PaTsa.Conference.App.Api.WebApi.Configuration;
+
+public static class ConfigurationKeys
+{
+    public const string MongoDbConnectionString = "MongoDb";
+
+    public const string SubmissionsDatabaseSection = "ConferenceDatabase";
+}
