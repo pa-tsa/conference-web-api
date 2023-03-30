80b0fe4c464daf4e1a228b5af3521e4b81bd9fa7
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-03-30 09:18:47 -0400

    Updated StatusController
    Added checks for ConferenceEventsService

--- src/PaTsa.Conference.App.Api.UnitTests/WebApi/Controllers/StatusControllerTests.cs
+++ src/PaTsa.Conference.App.Api.UnitTests/WebApi/Controllers/StatusControllerTests.cs
@@ -17,7 +17,7 @@ public class StatusControllerTests
 {
     private static void AssertServiceStatus(ServicesStatusModel servicesStatusModel, PingableServiceFailures pingableServiceFailures)
     {
-        throw new NotImplementedException();
+        Assert.Equal(servicesStatusModel.ConferenceEventsServiceIsAlive, !pingableServiceFailures.HasFlag(PingableServiceFailures.ConferenceEvents));
     }
 
     private static IList<IPingableService> BuildHealthyPingableServices()
@@ -94,7 +94,7 @@ public class StatusControllerTests
 
     #region Inline Data
 
-    [InlineData(PingableServiceFailures.None)]
+    [InlineData(PingableServiceFailures.ConferenceEvents)]
 
     #endregion
 
@@ -130,7 +130,7 @@ public class StatusControllerTests
 
     #region Inline Data
 
-    [InlineData(PingableServiceFailures.None)]
+    [InlineData(PingableServiceFailures.ConferenceEvents)]
 
     #endregion
 
@@ -165,7 +165,7 @@ public class StatusControllerTests
 
     private static Type[] GetServiceTypes()
     {
-        return Type.EmptyTypes;
+        return new[] { typeof(ConferenceEventsService) };
     }
 
     [Fact]
@@ -334,7 +334,8 @@ public class StatusControllerTests
 [Flags]
 public enum PingableServiceFailures
 {
-    None = 0
+    None = 0,
+    ConferenceEvents = 1 << 0
 }
 
 public enum ServiceFailureType
--- src/PaTsa.Conference.App.Api.WebApi/Models/ServicesStatusModel.cs
+++ src/PaTsa.Conference.App.Api.WebApi/Models/ServicesStatusModel.cs
@@ -1,6 +1,19 @@
-﻿namespace PaTsa.Conference.App.Api.WebApi.Models;
+﻿using System.Linq;
+
+namespace PaTsa.Conference.App.Api.WebApi.Models;
 
 public class ServicesStatusModel
 {
-    public bool IsHealthy => true;
+    public bool ConferenceEventsServiceIsAlive { get; set; }
+
+    public bool IsHealthy => EvaluateIsHealthy();
+
+    private bool EvaluateIsHealthy()
+    {
+        var propertyInfos = GetType().GetProperties().Where(_ => _.CanWrite && _.PropertyType == typeof(bool));
+
+        var isAliveList = propertyInfos.Select(propertyInfo => (bool)propertyInfo.GetValue(this)!).ToList();
+
+        return isAliveList.All(isAlive => isAlive);
+    }
 }
