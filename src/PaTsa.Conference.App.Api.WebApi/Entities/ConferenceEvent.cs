4b85b31c6b475de17c4c5a439a98ce516356e9fc
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-03-30 09:07:53 -0400

    Created ConferenceEvent entity and model
    Added extension methods to assist in converting between the two

---
+++ src/PaTsa.Conference.App.Api.WebApi/Entities/ConferenceEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PaTsa.Conference.App.Api.WebApi.Entities;
+
+public class ConferenceEvent : IMongoDbEntity
+{
+    public string? Description { get; set; }
+
+    public DateTime EndDateTime { get; set; }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public string? Location { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime StartDateTime { get; set; }
+
+    public string? Type { get; set; }
+}
