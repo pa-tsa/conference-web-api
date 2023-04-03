2067de466d3731422bedc8cb283ce3f244dca8a2
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-04-03 08:31:59 -0400

    Added additional services to application
    Added MongoClient
    Added ConfigurationDatabase config

--- src/PaTsa.Conference.App.Api.WebApi/Startup.cs
+++ src/PaTsa.Conference.App.Api.WebApi/Startup.cs
@@ -6,78 +6,85 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using PaTsa.Conference.App.Api.WebApi.Configuration;
 using PaTsa.Conference.App.Api.WebApi.Middleware;
 using PaTsa.Conference.App.Api.WebApi.Services;
 using PaTsa.Conference.App.Api.WebApi.Swagger;
 
-namespace PaTsa.Conference.App.Api.WebApi
+namespace PaTsa.Conference.App.Api.WebApi;
+
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; }
+        Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseMiddleware<ApiKeyMiddleware>();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Register Configuration
+        services.Configure<ConferenceDatabase>(Configuration.GetSection(ConfigurationKeys.ConferenceDatabaseSection));
+
+        // MongoDB Client
+        services.Add(
+            new ServiceDescriptor(typeof(IMongoClient),
+                new MongoClient(Configuration.GetConnectionString(ConfigurationKeys.MongoDbConnectionString))));
+
+        // MongoDB Services
+        services.AddSingleton<IConferenceEventsService, ConferenceEventsService>();
+
+        // Pingable Services - Should match MongoDB Services
+        services.AddSingleton<IPingableService, ConferenceEventsService>();
+
+        // Validators;
+
+        // Setup controllers
+        services
+            .AddControllers()
+            .AddNewtonsoftJson(options =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+            });
 
-            app.UseHttpsRedirection();
+        // Add Fluent Validation
+        //services.AddFluentValidationAutoValidation();
 
-            app.UseRouting();
+        //services.AddEndpointsApiExplorer();
 
-            app.UseAuthorization();
+        services
+            .AddSwaggerGen(options =>
+            {
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 
-            app.UseMiddleware<ApiKeyMiddleware>();
-
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //services.Configure<PointOfSalesDatabase>(Configuration.GetSection(ConfigurationKeys.ConferenceDatabaseSection));
-
-            // MongoDB Services
-            services.AddSingleton<IConferenceEventsService, ConferenceEventsService>();
-
-            // Pingable Services - Should match MongoDB Services
-            services.AddSingleton<IPingableService, ConferenceEventsService>();
-
-            // Validators;
-
-            // Setup controllers
-            services
-                .AddControllers()
-                .AddNewtonsoftJson(options =>
-                {
-                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
-                });
-
-            // Add Fluent Validation
-            //services.AddFluentValidationAutoValidation();
-
-            //services.AddEndpointsApiExplorer();
-
-            services
-                .AddSwaggerGen(options =>
-                {
-                    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-
-                    options.OperationFilter<ApiKeyHeaderFilter>();
-                })
-                .AddSwaggerGenNewtonsoftSupport();
-        }
+                options.OperationFilter<ApiKeyHeaderFilter>();
+            })
+            .AddSwaggerGenNewtonsoftSupport();
     }
 }
